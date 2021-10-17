bc1eb6f860cce8997cf6d2ef80a665e31dd6b897
kindnemo <namanmishraepicstar@gmail.com>
2021-10-17 19:59:41 +0530

    Finally added the code for second countdown but it is all broken and Idk what my code is doing exactly but I'm happy that I made it work. I'll scrap this and will work with something better

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let day, hour, minute, second;
 const setter = document.querySelector("#setBtn");
+let pSec = document.getElementById("second-count"),
+pMin = document.getElementById("minute-count");
+const setToTime = new Date(`1/${day}/2000`);
+// setToTime.setHours(hour);
+// setToTime.setMinutes(minute);
+// setToTime.setSeconds(second);
+
+
 
 function assign(ev){
     
@@ -8,6 +16,30 @@ function assign(ev){
     hour = document.getElementById("hour-set").value;
     minute = document.getElementById("minute-set").value;
     second = document.getElementById("second-set").value;
+    
+    setInterval(secondChange, 1000);
+    setInterval(minuteChnage, 60000);
+    
 }
 
+
+function secondChange(){     //The second timer
+    if(second>=0){
+        pSec.innerHTML = `${second}`
+    }else{
+        return;
+    }
+    second--;
+}
+
+
+function minuteChnage(){     //The Minute timer
+    if(minute>=0){
+        pMin.innerHTML = `${minute}`;
+    }else{
+        return;
+    }
+    minute--
+}
 setter.addEventListener("click", assign);
+
